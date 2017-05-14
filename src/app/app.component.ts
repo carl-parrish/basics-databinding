d3b18fc3c6a56dd55e7f6963fb864175e710c45a
Carl Parrish <cparrish@glynlyon.com>
2017-05-14 14:14:19 -0700

    Inital checkin" "

---
+++ src/app/app.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  // styleUrls: ['./app.component.css']
+  styles: [`
+    h3 {
+      color: dodgerblue;
+    }
+  `]
+})
+export class AppComponent {
+  public userName = '';
+
+  public onResetUserName(name: Event): void {
+    this.userName = '';
+  }
+}
